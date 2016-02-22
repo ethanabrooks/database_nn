2de8bad5a5c24b357e5f3bf7027bbea91d9642a2
Ethan <ethanabrooks@gmail.com>
2016-02-22 08:49:12 -0500

    added import statement

--- qa_wiki_parser.py
+++ qa_wiki_parser.py
@@ -1,164 +1,172 @@
+from __future__ import print_function
+
 import xml.sax
 import mwparserfromhell
 import sys
 from spacy.en import English
 from wiki_nlp_util import *
 
+
 # qa_object is an object that encompasses the question/answer pair
 class qa_object:
-	def __init__(self, question, answer, entity):
-		self.question = question
-		self.answer = answer
-		self.entity = entity
-		self.correct_sents = []
-		self.wrong_sents = []
+    def __init__(self, question, answer, entity):
+        self.question = question
+        self.answer = answer
+        self.entity = entity
+        self.correct_sents = []
+        self.wrong_sents = []
 
-	def store_correct_sents(self, sents):
-		self.correct_sents = sents
+    def store_correct_sents(self, sents):
+        self.correct_sents = sents
 
-	def store_wrong_sents(self, sents):
-		self.wrong_sents = sents
+    def store_wrong_sents(self, sents):
+        self.wrong_sents = sents
 
-	# Testing purposes
-	def print(self):
-		print(
-				'Question: ' + self.question + 
-				' \n Answer: ' + self.answer +
-				' \n Entity: ' + self.entity
-			 )
+    # Testing purposes
+    def print(self):
+        print(
+            'Question: ' + self.question +
+            ' \n Answer: ' + self.answer +
+            ' \n Entity: ' + self.entity
+        )
 
-	def get_qa_string(self):
-		if len(self.correct_sents) == 0 or len(self.wrong_sents) == 0:
-			return ''
-		output = self.question + ',' + self.answer + ',' + str(len(self.correct_sents)) +  ',' + str(len(self.wrong_sents))
-		# Only print first correct sentence
-		for s in self.correct_sents:
-			output += '\n' + s
-			break
-		index = 1
-		for s in self.wrong_sents:
-			output += '\n' + str(index) + '. ' + s
-			index += 1
-		return output
+    def get_qa_string(self):
+        if len(self.correct_sents) == 0 or len(self.wrong_sents) == 0:
+            return ''
+        output = self.question + ',' + self.answer + ',' + str(len(self.correct_sents)) + ',' + str(
+            len(self.wrong_sents))
+        # Only print first correct sentence
+        for s in self.correct_sents:
+            output += '\n' + s
+            break
+        index = 1
+        for s in self.wrong_sents:
+            output += '\n' + str(index) + '. ' + s
+            index += 1
+        return output
+
 
 # Takes in a file where every line contains the question and answer separated by a '|'
 # Returns a dictionary where the key is the entity extracted, and the value is the qa_object
 # nlp_parser is the spacy nlp parser object
 def read_question_answer_pair(filename, nlp_parser):
-	with open(filename) as f:
-		lines = f.read().splitlines()
-	d = {}
-	for line in lines:
-		l = line.split('|')
-		entities = get_ents_from_string(nlp_parser(l[0]))
-		for entity in entities:
-			# does not account for the fact that the same entity can appear in different questions!
-			# Also entities are indexed by their lowercase forms (not case sensitive)
-			entity_name = entity.text
-			d[entity_name.lower()] = qa_object(l[0], l[1], entity_name)
-	return d
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    d = {}
+    for line in lines:
+        l = line.split('|')
+        entities = get_ents_from_string(nlp_parser(l[0]))
+        for entity in entities:
+            # does not account for the fact that the same entity can appear in different questions!
+            # Also entities are indexed by their lowercase forms (not case sensitive)
+            entity_name = entity.text
+            d[entity_name.lower()] = qa_object(l[0], l[1], entity_name)
+    return d
+
 
 # Class that extends ContentHandler
 # Ignores cases when comparing to document
 class WikiContentHandler(xml.sax.ContentHandler):
-	def __init__(self, nlp_parser, entity_pairs):
-		xml.sax.ContentHandler.__init__(self)
-		self.nlp_parser = nlp_parser
-		# entity_answer pairs from read_entity_answer_pair function
-		self.entity_pairs = entity_pairs
-		
-		# Page check
-		self.page_flag = False
-		# Title check
-		self.title_flag = False
-		# Text check
-		self.text_flag = False
-		# Entity matched
-		self.entity_flag = False
+    def __init__(self, nlp_parser, entity_pairs):
+        xml.sax.ContentHandler.__init__(self)
+        self.nlp_parser = nlp_parser
+        # entity_answer pairs from read_entity_answer_pair function
+        self.entity_pairs = entity_pairs
 
-		# Keep track of the current matched entity (title) and text
-		self.current_entity = ''
-		self.current_entity_text = ''
+        # Page check
+        self.page_flag = False
+        # Title check
+        self.title_flag = False
+        # Text check
+        self.text_flag = False
+        # Entity matched
+        self.entity_flag = False
 
-	# Removes wikipedia markup using external library
-	def clean_markup(self, content):
-		clean_code = mwparserfromhell.parse(content).strip_code()
-		return clean_code
+        # Keep track of the current matched entity (title) and text
+        self.current_entity = ''
+        self.current_entity_text = ''
 
-	# Updates the entity/object pair by storing the correct/wrong sentence results
-	def update_entity_from_wiki_text(self):
-		wiki_text = self.clean_markup(self.current_entity_text)
-		parsed_wiki_text = self.nlp_parser(wiki_text)
+    # Removes wikipedia markup using external library
+    def clean_markup(self, content):
+        clean_code = mwparserfromhell.parse(content).strip_code()
+        return clean_code
 
-		qa_object = self.entity_pairs[self.current_entity]
+    # Updates the entity/object pair by storing the correct/wrong sentence results
+    def update_entity_from_wiki_text(self):
+        wiki_text = self.clean_markup(self.current_entity_text)
+        parsed_wiki_text = self.nlp_parser(wiki_text)
 
-		correct_sents = filter_sentences(parsed_wiki_text, qa_object.answer)
-		incorrect_sents = fetch_random_sentences(parsed_wiki_text, qa_object.answer, 15)
-		qa_object.store_correct_sents(correct_sents)
-		qa_object.store_wrong_sents(incorrect_sents)
+        qa_object = self.entity_pairs[self.current_entity]
 
-		self.entity_pairs[self.current_entity] = qa_object
-		return
+        correct_sents = filter_sentences(parsed_wiki_text, qa_object.answer)
+        incorrect_sents = fetch_random_sentences(parsed_wiki_text, qa_object.answer, 15)
+        qa_object.store_correct_sents(correct_sents)
+        qa_object.store_wrong_sents(incorrect_sents)
 
-	def startElement(self, name, attrs):
-		if name == 'page':
-			self.page_flag = True
-		elif name == 'title':
-			self.title_flag = True
-		elif name == 'text':
-			self.text_flag = True
+        self.entity_pairs[self.current_entity] = qa_object
+        return
 
-	def endElement(self, name):
-		if name == 'page':
-			self.page_flag = False
+    def startElement(self, name, attrs):
+        if name == 'page':
+            self.page_flag = True
+        elif name == 'title':
+            self.title_flag = True
+        elif name == 'text':
+            self.text_flag = True
 
-			# If entity has been matched, reset state
-			if self.entity_flag:
-				self.update_entity_from_wiki_text()
-				self.entity_flag = False
-				self.current_entity = ''
-				self.current_entity_text = ''
-		elif name == 'title':
-			self.title_flag = False
-		elif name == 'text':
-			self.text_flag = False
+    def endElement(self, name):
+        if name == 'page':
+            self.page_flag = False
 
-	def characters(self, content):
-		if self.title_flag:
-			title = content.lower()
-			# the keys in entity_pairs are already lowercase
-			if title in self.entity_pairs:
-				self.entity_flag = True
-				self.current_entity = title
-		elif self.text_flag:
-			if self.entity_flag:
-				self.current_entity_text += content
+            # If entity has been matched, reset state
+            if self.entity_flag:
+                self.update_entity_from_wiki_text()
+                self.entity_flag = False
+                self.current_entity = ''
+                self.current_entity_text = ''
+        elif name == 'title':
+            self.title_flag = False
+        elif name == 'text':
+            self.text_flag = False
 
-if __name__=='__main__':
-	if len(sys.argv) < 4:
-		print('Not enough arguments given', file=sys.stderr)
-		sys.exit()
+    def characters(self, content):
+        if self.title_flag:
+            title = content.lower()
+            # the keys in entity_pairs are already lowercase
+            if title in self.entity_pairs:
+                self.entity_flag = True
+                self.current_entity = title
+        elif self.text_flag:
+            if self.entity_flag:
+                self.current_entity_text += content
 
-	input_filename = sys.argv[1]
-	xml_sourcename = sys.argv[2]
-	output_filename = sys.argv[3]
 
-	nlp_parser = English()
-	d = read_question_answer_pair(input_filename, nlp_parser)
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print('Not enough arguments given', file=sys.stderr)
+        sys.exit()
 
-	print('Entities found: \n')
-	for x in d:
-		print(x)
+    input_filename = sys.argv[1]
+    xml_sourcename = sys.argv[2]
+    output_filename = sys.argv[3]
 
-	source = open(xml_sourcename)
-	handler = WikiContentHandler(nlp_parser, d)
-	xml.sax.parse(source, handler)
+    nlp_parser = English()
+    d = read_question_answer_pair(input_filename, nlp_parser)
 
-	pairs = handler.entity_pairs
+    print('Entities found: \n')
+    for x in d:
+        print(x)
 
-	output = open(output_filename, 'a')
+    source = open(xml_sourcename)
+    handler = WikiContentHandler(nlp_parser, d)
+    xml.sax.parse(source, handler)
 
-	for entity in pairs:
-		output.write(pairs[entity].get_qa_string() + '\n')
+    pairs = handler.entity_pairs
 
-	source.close()
+    output = open(output_filename, 'a')
+
+    for entity in pairs:
+        output.write(pairs[entity].get_qa_string() + '\n')
+
+    print("all done")
+    source.close()
