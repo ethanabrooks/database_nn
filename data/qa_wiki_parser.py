5aae80a3f3f6b08aec72af37f44e6db6f3faa24d
Benson Chen <benatorc@gmail.com>
2016-03-25 01:54:46 -0400

    update parser

--- data/qa_wiki_parser.py
+++ data/qa_wiki_parser.py
@@ -7,6 +7,40 @@ import io
 from spacy.en import English
 from wiki_nlp_util import *
 
+class jeopardy_obj:
+    def __init__(self, question, answer, entities):
+        self.question = question
+        self.answer = answer
+        self.entities = entities
+        self.correct_sents = []
+        self.wrong_sents = []
+        self.has_matched = False
+
+    def store_correct_sents(self, sents):
+        self.correct_sents = sents
+
+    def store_wrong_sents(self, sents):
+        self.wrong_sents = sents
+
+    # no line breaks
+    def get_clean_sentence(self, s):
+        return s.replace('\n', ' ').replace('\r', '').strip()
+
+    def get_qa_string(self):
+        if len(self.correct_sents) == 0 or len(self.wrong_sents) == 0:
+            return ''
+        output = self.question
+        output += '\n' + self.answer
+        # Only print first correct sentence
+        for s in self.correct_sents:
+            output += '\n' + self.get_clean_sentence(s)
+            break
+        output += '\n' + str(len(self.wrong_sents))
+        index = 1
+        for s in self.wrong_sents:
+            output += '\n' + self.get_clean_sentence(s)
+            index += 1
+        return output
 
 # qa_object is an object that encompasses the question/answer pair
 class qa_object:
@@ -58,16 +92,21 @@ class qa_object:
 def read_question_answer_pair(filename, nlp_parser):
     with open(filename) as f:
         lines = f.read().splitlines()
-    d = {}
+    d = []
     for line in lines:
-        decoded_line = line.decode('utf-8').strip()
+        decoded_line = line.strip()
         l = decoded_line.split('|')
+        question = l[0]
+        answer = l[1]
         entities = get_ents_from_string(nlp_parser(l[0]))
-        for entity in entities:
-            # does not account for the fact that the same entity can appear in different questions!
-            # Also entities are indexed by their lowercase forms (not case sensitive)
-            entity_name = entity.text
-            d[entity_name.lower()] = qa_object(l[0], l[1], entity_name)
+        # stores a list of the entity texts
+        entities = [ent.text for ent in entities]
+        d.append(jeopardy_obj(question, answer, entities))
+        # for entity in entities:
+        #     # does not account for the fact that the same entity can appear in different questions!
+        #     # Also entities are indexed by their lowercase forms (not case sensitive)
+        #     entity_name = entity.text
+        #     d[entity_name.lower()] = qa_object(l[0], l[1], entity_name)
     return d
 
 
@@ -80,6 +119,8 @@ class WikiContentHandler(xml.sax.ContentHandler):
         # entity_answer pairs from read_entity_answer_pair function
         self.entity_pairs = entity_pairs
 
+        self.current_matched_obj = None
+
         # Page check
         self.page_flag = False
         # Title check
@@ -90,7 +131,6 @@ class WikiContentHandler(xml.sax.ContentHandler):
         self.entity_flag = False
 
         # Keep track of the current matched entity (title) and text
-        self.current_entity = ''
         self.current_entity_text = ''
 
     # Removes wikipedia markup using external library
@@ -103,16 +143,13 @@ class WikiContentHandler(xml.sax.ContentHandler):
         wiki_text = self.clean_markup(self.current_entity_text)
         parsed_wiki_text = self.nlp_parser(wiki_text)
 
-        qa_object = self.entity_pairs[self.current_entity]
-
-        correct_sents = filter_sentences(parsed_wiki_text, qa_object.answer)
+        correct_sents = filter_sentences(parsed_wiki_text, self.current_matched_obj.answer)
         incorrect_sents = []
         if correct_sents != []:
-            incorrect_sents = fetch_random_sentences(parsed_wiki_text, qa_object.answer, 15)
-        qa_object.store_correct_sents(correct_sents)
-        qa_object.store_wrong_sents(incorrect_sents)
-
-        self.entity_pairs[self.current_entity] = qa_object
+            incorrect_sents = fetch_random_sentences(parsed_wiki_text, self.current_matched_obj.answer, 15)
+            self.current_matched_obj.has_matched = True
+            self.current_matched_obj.store_correct_sents(correct_sents)
+            self.current_matched_obj.store_wrong_sents(incorrect_sents)
         return
 
     def startElement(self, name, attrs):
@@ -131,8 +168,7 @@ class WikiContentHandler(xml.sax.ContentHandler):
             if self.entity_flag:
                 self.update_entity_from_wiki_text()
                 self.entity_flag = False
-                self.current_entity = ''
-                self.current_entity_text = ''
+                self.current_matched_obj = ''
         elif name == 'title':
             self.title_flag = False
         elif name == 'text':
@@ -141,10 +177,15 @@ class WikiContentHandler(xml.sax.ContentHandler):
     def characters(self, content):
         if self.title_flag:
             title = content.lower()
-            # the keys in entity_pairs are already lowercase
-            if title in self.entity_pairs:
-                self.entity_flag = True
-                self.current_entity = title
+            for obj in self.entity_pairs:
+                #Skip objects already matched
+                if obj.has_matched:
+                    continue
+                for ent in obj.entities:
+                    if ent.lower().strip() in title.strip():
+                        self.entity_flag = True
+                        self.current_matched_obj = obj
+                        break
         elif self.text_flag:
             if self.entity_flag:
                 self.current_entity_text += content
@@ -158,21 +199,27 @@ if __name__ == '__main__':
     input_filename = sys.argv[1]
     xml_sourcename = sys.argv[2]
     output_filename = sys.argv[3]
+    output2_filename = sys.argv[4]
 
     nlp_parser = English()
     d = read_question_answer_pair(input_filename, nlp_parser)
-
+    print('finished reading')
     source = open(xml_sourcename)
     handler = WikiContentHandler(nlp_parser, d)
     xml.sax.parse(source, handler)
 
     pairs = handler.entity_pairs
 
-    output = io.open(output_filename, 'w', encoding='utf-8')
+    print('len: ' + str(len(pairs)))
 
-    for entity in pairs:
-        entity_string = pairs[entity].get_qa_string()
-        if entity_string != '':
-            output.write(pairs[entity].get_qa_string() + '\n')
-    print("all done")
+    print('finished parsing')
+    output = io.open(output_filename, 'w+', encoding='utf-8')
+    output_2 = io.open(output2_filename, 'w+', encoding='utf-8')
+
+    for obj in pairs:
+        if obj.has_matched:
+            s = obj.get_qa_string()
+            output.write(s + '\n')
+        else:
+            output_2.write(obj.question + '\n' + 'Not matched' + '\n')
     source.close()
--- data/run_parser.sh
+++ data/run_parser.sh
@@ -3,10 +3,10 @@ export PATH=/nlp/pkg/bin:$PATH
 
 export PYTHONPATH=/nlp/pkg/sw/spacy/lib/python2.7/site-packages:/nlp/pkg/lib64/python2.7/site-packages:/nlp/pkg/lib/python2.7/site-packages:/nlp/pkg/sw/stanford_corenlp_pywrapper/lib/python2.7/site-packages:/nlp/pkg/sw/nltk/lib/python2.7/site-packages:/nlp/pkg/sw/mwparserfromhell/lib64/python2.7/site-packages
 
-export JOB_PATH=/home1/b/bensonc/database_nn
+export JOB_PATH=/home1/b/bensonc/database_nn/data
 
 date
 
-python $JOB_PATH/qa_wiki_parser.py $JOB_PATH/question_answer_pairs /scratch-shared/users/bensonc/enwiki-20160204-pages-meta-current.xml $JOB_PATH/output_nn
+python $JOB_PATH/qa_wiki_parser.py $JOB_PATH/question_answer_pairs /scratch-shared/users/bensonc/enwiki-20160204-pages-meta-current.xml $JOB_PATH/output_nn_new $JOB_PATH/question_status
 
 date
