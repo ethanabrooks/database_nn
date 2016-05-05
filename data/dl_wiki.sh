f578a7620344308e80146d969c1da608d8132755
Ethan <ethanabrooks@gmail.com>
2016-05-05 15:05:23 -0400

    gitignored all the data

--- data/dl_wiki.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget https://dumps.wikimedia.org/enwiki/20160204/enwiki-20160204-pages-meta-current.xml.bz2 -P /scratch-shared/users/bensonc
--- data/run_main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in `seq 0 49`;
-    do
-    	qsub -v RUN_NUMBER=$i run_parser.sh
-    done
